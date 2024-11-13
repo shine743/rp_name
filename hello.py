a2e7a6f52908432dd479785b9cc3ee6ec2e3ec4f
shine <chr70145@gmail.com>
2024-11-13 16:26:50 +0900

    포트 맞춤

--- hello.py
+++ hello.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 def home():
     return 'hello.'
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=4000)
-# http://127.0.0.1:4000/
+    app.run(debug=True, host='0.0.0.0', port=5000)
+# http://127.0.0.1:5000/
