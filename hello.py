8f89dff93497f278b5e364b2dec4ebeaf3ab0ac4
shine <chr70145@gmail.com>
2024-11-13 16:14:27 +0900

    플라스크

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-print('hello')
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return 'hello.'
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=4000)
+# http://127.0.0.1:4000/
